4253e2043d929e5b88c696f5a95fd761eab9af18
KiriII <Kulagin1999@list.ru>
2018-03-15 13:10:49 +0300

    Menu
    SHO?

---
+++ Survival_Polytech/Assets/MENU/GO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GO : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+#pragma warning disable CS0618 // Тип или член устарел
+        Application.LoadLevel("Main");
+#pragma warning restore CS0618 // Тип или член устарел
+    }
+}
